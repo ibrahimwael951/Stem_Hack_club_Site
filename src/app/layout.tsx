69d861b546bb762078e1691e03a2d3a51a8c2692
ibrahimwael951 <ibrahimwael809@gmail.com>
2025-04-22 16:45:44 +0200

    remove seo code from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
-import type { Metadata } from "next";
+ 
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
-export const metadata: Metadata = {
-  title: "Stem Hack Club",
-  description:
-    " Stem Hack Club is a vibrant student-led community where young minds explore, learn, and build projects in STEM fields—especially coding, technology, and innovation",
-};
+ 
 
 export default function RootLayout({
   children,
