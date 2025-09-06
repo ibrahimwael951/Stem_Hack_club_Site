bffc7d255c2a98cc33d3d09c6265d31c942145c6
ibrahimwael951 <ibrahimwael809@gmail.com>
2025-09-06 16:21:59 +0300

    added Games file

---
+++ src/app/Games/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <main>
+      
+    {/* type all your Iframe here then i will make the rest ("-") */}
+
+
+    </main>
+  )
+}
